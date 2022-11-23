cd6246d77640a3cf1bd1b17c8718b516db41d850
Mickey <mickeyc11111@gmail.com>
2022-11-22 20:19:25 -0500

    client includes memberships, DONE

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,4 +1,12 @@
 class Client < ApplicationRecord
     has_many :memberships
     has_many :gyms, through: :memberships
+
+    def total_charges
+        total = 0
+        memberships = self.memberships
+        memberships.map {|t| total += t.charge }
+        
+        total
+    end
 end
